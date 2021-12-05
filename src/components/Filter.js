dc2bdda48f1666f1430dd49eb75c49923e420b5e
Marlena Tsang <Marlena345@gmail.com>
2021-12-05 16:45:51 -0500

    Done.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onSearchChange, onCategoryChange }) {
+  function handleSearchChange(event){
+    onSearchChange(event.target.value)
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+      type="text" 
+      name="search"
+      placeholder="Search..." 
+      onChange={handleSearchChange} 
+      value={search}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
@@ -15,3 +25,10 @@ function Filter({ onCategoryChange }) {
 }
 
 export default Filter;
+{/* ✕ uses a prop of 'search' to display the search term in the input field (124 ms)
+    ✕ calls the onSearchChange callback prop when the input is changed (9 ms)
+    ✕ the input field acts as a controlled input (42 ms)
+    ✕ the shopping list displays all items when initially rendered (24 ms)
+    ✕ the shopping filters based on the search term to include full matches (31 ms)
+    ✕ the shopping filters based on the search term to include partial matches (137 ms)
+*/}
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [category, setCategory] = useState("Produce");
+  const [name, setName] = useState("");
+
+  function handleCategoryChange(event){
+    setCategory(event.target.value)
+  }
+
+  function handleNameChange(event){
+    setName(event.target.value)
+  }
+
+    function handleSubmit(event){
+      event.preventDefault();
+      onItemFormSubmit({
+        id: uuid(), // the `uuid` library can be used to generate a unique id
+        name,
+        category,
+      }) 
+    }
+  
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+        type="text" 
+        name="name"
+        value={name}
+        onChange={handleNameChange}
+         />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+        name="category"
+        value={category}
+        onChange={handleCategoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
